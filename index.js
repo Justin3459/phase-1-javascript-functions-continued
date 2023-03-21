eb22a6303780ea58e68bff2eedc41650a136f9bc
justin becker <justin3459@hotmail.com>
2023-03-21 15:23:16 -0400

    Created functions to return certain texts and set default values to parameters

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity) {
+        activity = typeof activity !== 'undefined' ? activity : 'roller-skate'
+        return  `This Saturday, I want to ${activity}!`
+}
+
+    function mondayWork(monday){
+        monday = typeof monday !== 'undefined' ? monday : 'go to the office'
+        return `This Monday, I will ${monday}.`
+}   
+
+function wrapAdjective(special = "*") {
+    return function (adjective = "special") {
+            return `You are ${special}${adjective}${special}!`;
+    }
+}
